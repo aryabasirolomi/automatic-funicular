582044f9e87f832b283a1eaca189460b257b6b4c
Arya Basirolomi <aryabasir2001@hotmail.com>
2020-10-24 14:47:47 -0400

    Copied Aric, added simple networl

---
+++ simple.py
@@ -0,0 +1,11 @@
+"""A simple social network f
+or tracking cononectiong between people"""
+class Person:
+    """A person in the social network
+    
+    Attributes:
+        name(str): person name
+        connections (set of Person): other people in the
+            social network who know this person
+    """
+    
